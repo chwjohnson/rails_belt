c1661c953b24dc07d0842ba8885f52429bab5bd8
Clinton Johnson <chwjohnson@gmail.com>
2016-06-24 17:22:10 -0500

    deployment

---
+++ app/models/lender.rb
@@ -0,0 +1,8 @@
+class Lender < ActiveRecord::Base
+  has_secure_password
+  has_many :joins
+  has_many :lenders, through: :joins
+  validates :email, :password, :password_confirmation, :first_name, :last_name, :money, presence: true
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+end
